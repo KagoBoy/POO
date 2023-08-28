76ae0c90f9ced63023740df6e8ac5222485bf1c8
Yan <61756965+KagoBoy@users.noreply.github.com>
2023-08-28 11:33:50 -0300

    Update Room.java

--- zuul-bad/Room.java
+++ zuul-bad/Room.java
@@ -64,4 +64,9 @@ public class Room
         return returnString;
     }
 
+    public String getLongDescription()
+    {
+        return "You are " + description + ".\n" + getExitString();
+    }
+
 }
